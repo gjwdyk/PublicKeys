fc22350eac03ce4a20105d258e8e8624a9646135
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-02-28 11:28:18 +0700

    Update

--- FetchUpdates.sh
+++ FetchUpdates.sh
@@ -9,62 +9,52 @@ declare -a file_acl
 declare -a file_own
 declare -a file_result
 
-file_url[9]="https://raw.githubusercontent.com/gjwdyk/PublicKeys/main/JigokuShoujo43x25.text"
-file_name[9]="$HOME/JigokuShoujo43x25.text"
-file_acl[9]="644"
-file_own[9]="ubuntu:ubuntu"
-
-file_url[0]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/id_rsa"
-file_name[0]="$HOME/.ssh/id_rsa"
-file_acl[0]="600"
+file_url[0]="https://raw.githubusercontent.com/gjwdyk/PublicKeys/main/JigokuShoujo43x25.text"
+file_name[0]="$HOME/JigokuShoujo43x25.text"
+file_acl[0]="644"
 file_own[0]="ubuntu:ubuntu"
 
-file_url[1]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/id_rsa.pub"
-file_name[1]="$HOME/.ssh/id_rsa.pub"
+file_url[1]="https://apt.grafana.com/gpg.key](https://apt.grafana.com/gpg.key"
+file_name[1]="$HOME/AptGrafanaCom_GpgKey"
 file_acl[1]="644"
 file_own[1]="ubuntu:ubuntu"
 
-file_url[2]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/id_rsa.pub"
-file_name[2]="$HOME/.ssh/authorized_keys"
-file_acl[2]="600"
+file_url[2]="https://artifacts.elastic.co/GPG-KEY-elasticsearch"
+file_name[2]="$HOME/ArtifactsElasticCo_GpgKeyElasticsearch"
+file_acl[2]="644"
 file_own[2]="ubuntu:ubuntu"
 
-file_url[3]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/VMPrepOS.sh"
-file_name[3]="$HOME/PrepareOS.sh"
-file_acl[3]="777"
+file_url[3]="https://cs.nginx.com/static/files/90pkgs-nginx](https://cs.nginx.com/static/files/90pkgs-nginx"
+file_name[3]="$HOME/CsNginxCom_StaticFiles_90PkgsNginx"
+file_acl[3]="644"
 file_own[3]="ubuntu:ubuntu"
 
-file_url[4]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/VMPrepContainerRuntimeInterface.sh"
-file_name[4]="$HOME/PrepareContainerRuntimeInterface.sh"
-file_acl[4]="777"
+file_url[4]="https://cs.nginx.com/static/keys/app-protect-security-updates.key"
+file_name[4]="$HOME/CsNginxCom_StaticKeys_AppProtectSecurityUpdates.key"
+file_acl[4]="644"
 file_own[4]="ubuntu:ubuntu"
 
-file_url[5]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/VMPrepCommonK8s.sh"
-file_name[5]="$HOME/PrepareCommonK8s.sh"
-file_acl[5]="777"
+file_url[5]="https://cs.nginx.com/static/keys/nginx_signing.key"
+file_name[5]="$HOME/CsNginxCom_StaticKeys_NginxSigning.key"
+file_acl[5]="644"
 file_own[5]="ubuntu:ubuntu"
 
-file_url[6]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/VMPrepK8sMaster.sh"
-file_name[6]="$HOME/PrepareK8sMaster.sh"
-file_acl[6]="777"
+file_url[6]="https://download.docker.com/linux/ubuntu/gpg"
+file_name[6]="$HOME/DownloadDockerCom_LinuxUbuntu_gpg"
+file_acl[6]="644"
 file_own[6]="ubuntu:ubuntu"
 
-file_url[7]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/VMPrepK8sWorker.sh"
-file_name[7]="$HOME/PrepareK8sWorker.sh"
-file_acl[7]="777"
+file_url[7]="https://nginx.org/keys/nginx_signing.key"
+file_name[7]="$HOME/NginxOrg_Keys_NginxSigning.key"
+file_acl[7]="644"
 file_own[7]="ubuntu:ubuntu"
 
-file_url[8]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/PrepApplicationService.sh"
-file_name[8]="$HOME/PrepareApplicationService.sh"
-file_acl[8]="777"
+file_url[8]="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+file_name[8]="$HOME/PackagesCloudGoogleCom_AptDoc_AptKey.gpg"
+file_acl[8]="644"
 file_own[8]="ubuntu:ubuntu"
 
-file_url[9]="https://raw.githubusercontent.com/gjwdyk/NGINX-Notes/main/NMS-Instance-Manager/K8sServer/JigokuShoujo43x25.text"
-file_name[9]="$HOME/JigokuShoujo43x25.text"
-file_acl[9]="644"
-file_own[9]="ubuntu:ubuntu"
-
-max_counter=9
+max_counter=8
 
 URLRegEx="^(http:\/\/|https:\/\/)?[a-z0-9]+((\-|\.)[a-z0-9]+)*\.[a-z]{2,}(:[0-9]{1,5})?(\/.*)*$"
 
@@ -86,10 +76,6 @@ done
 #╔═══════════════════╗
 #║   Review Status   ║
 #╚═══════════════════╝
-
-kubectl get node -o wide
-kubectl get pod --all-namespaces -o wide
-kubectl get service --all-namespaces -o wide
 
 cat $HOME/JigokuShoujo43x25.text
 
